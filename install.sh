782e0fdf27eb55b76553f3dd01bb3653e435e765
Artur Boyun <arturboyun@gmail.com>
2024-05-21 18:26:41 +0300

    feat(install.sh): Add systemd service installation to install script

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
 #! /usr/bin/sh
 
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+BOLD=$(tput bold)
+NORMAL=$(tput sgr0)
+
 # Install dependencies
 [ ! -f ./venv ] && python -m venv venv
 
@@ -7,3 +16,16 @@ venv/bin/pip install -r requirements.txt
 clear
 # Run the application
 venv/bin/python -m obsidian_sync.install
+
+echo 
+echo "${GREEN}${BOLD}Installing the systemd service..."
+
+sudo cp systemd/obsidian_sync.service /etc/systemd/system/obsidian_sync.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable obsidian_sync
+sudo systemctl start obsidian_sync
+
+echo "Service started!${NORMAL}${NC}"
+echo
+echo "${GREEN}You can check the status of the service by running '${BLUE}${BOLD}sudo systemctl status obsidian_sync${NORMAL}${NC}'"
